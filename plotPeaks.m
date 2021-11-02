7b38b1f985a436f41f3ec663db65f36a4b39e3ba
ambrozin <46778101+ambrozin@users.noreply.github.com>
2021-11-02 17:01:26 +0100

    Initial commit two files for surf peaks and plot peaks has been added and commited. 	plotPeaks

---
+++ plotPeaks.m
@@ -0,0 +1,9 @@
+% this file is to test git hub with matlab integration 
+%% this section clears and closes all 
+
+clear all, close all, clc
+
+%% 
+
+figure(1) 
+plot(peaks)
---
+++ surfPeaks.m
@@ -0,0 +1,10 @@
+% this file is to test the git hub integration with matlab 
+
+%% this section clears the workspace and closes all figure windows 
+
+clc, clear all, close all
+
+%% 
+
+figure(1)
+surf(peaks), shading interp 
